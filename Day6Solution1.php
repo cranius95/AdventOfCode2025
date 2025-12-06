ce416aa939ab7e40b7d876caf3450f4d0c8ebdfc
Luca <indre.luca@gmail.com>
2025-12-06 17:13:46 +0200

    day 6 solution 1

---
+++ Day6Solution1.php
@@ -0,0 +1,43 @@
+<?php
+// $inputs = file_get_contents();
+$inputs = '123 328  51 64 
+ 45 64  387 23 
+  6 98  215 314
+*   +   *   + ';
+  
+$count = 0;
+$inputs = explode(PHP_EOL,$inputs);
+$inputs = array_map(function($val) {
+	 $val = trim(preg_replace('/ +/', ' ', $val));
+    return explode(' ',$val);
+}, $inputs);
+$flipped = [];
+$length = count($inputs);
+$width = count($inputs[0]);
+for($i=0;$i<=$length-1;$i++){
+	for($j=0;$j<=$width-1;$j++){
+		$flipped[$j][$i] = $inputs[$i][$j];
+	}
+}
+
+foreach($flipped as $input){
+	
+	switch($input[$length-1]){
+		case "+":
+			$partial = 0;
+			for($i=0;$i<=$length-2;$i++){
+				$partial = $partial+ (int)$input[$i];
+				
+			}
+			break;
+			case "*":
+				$partial = 1;
+					for($i=0;$i<=$length-2;$i++){
+				$partial = $partial * $input[$i];
+						
+					}
+				break;
+	}
+	$count +=$partial;
+}
+print_r($count);
